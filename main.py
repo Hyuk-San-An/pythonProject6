86c1f8cce6e464f175e1622aa03b907fb576cb5b
Hyuk-San-An <ahn9344@naver.com>
2023-02-11 23:01:27 +0900

    plus

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ list1=[1,2,3,4,5]
 list2=[6,7,8,9]
 print(list1[0])
 
-def multi(a,b):
+def plus(a,b):
     return a+b
 
-def minus(a,b):
-    return a-b
-
-def plus(a,b):
-    return a*b
-
-print(list1[-1])
